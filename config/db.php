d208da62ad4e204fe6af404a34dcf05a3f03a99e
Theodore Temourtzidis <thodoristem@users.noreply.github.com>
2018-01-21 11:48:12 +0200

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,12 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=currency_calc_db',
-    'username' => 'root',
-    'password' => '050490tt',
+    'dsn' => 'mysql:host=localhost;dbname=dbname',
+    'username' => 'dbuser',
+    'password' => 'dbpass',
     'charset' => 'utf8',
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
 ];
